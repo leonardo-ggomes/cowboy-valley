95134b545e3512ed29a4ca8ef65f4fe099664c64
Leonardo Garcia <leonardogarciaoficial1@gmail.com>
2025-11-04 18:47:33 -0300

    Create project

---
+++ src/Experience.ts
@@ -0,0 +1,237 @@
+import { BufferGeometry, Clock, DirectionalLight, DirectionalLightHelper, MathUtils, Mesh, Quaternion, Raycaster, Vector3 } from "three"
+import Camera from "./Camera"
+import MainScene from "./MainScene"
+import Renderer from "./Renderer"
+import Player from "./Player"
+import Loader from "./Loader"
+import { acceleratedRaycast, computeBoundsTree, disposeBoundsTree } from "three-mesh-bvh"
+import Stats from "three/examples/jsm/libs/stats.module.js"
+import NPCManager from "./NPCManager"
+
+
+BufferGeometry.prototype.computeBoundsTree = computeBoundsTree
+BufferGeometry.prototype.disposeBoundsTree = disposeBoundsTree
+Mesh.prototype.raycast = acceleratedRaycast
+
+class Experience {
+
+    camera: Camera
+    mainScene: MainScene
+    renderer: Renderer
+    clock: Clock
+    player: Player
+    loader: Loader
+    collisionMeshes: Mesh[] = []
+    raycaster = new Raycaster()
+    down = new Vector3(0, -1, 0)
+    keysPressed = new Set<string>()
+    velocityY = 0
+    gravity = -9.81
+    playerDirection = new Vector3()
+    quaternion = new Quaternion()
+    playerImpulse = new Vector3()
+    velocity = 1.4;
+    hit = false
+    npcManager: NPCManager
+
+    stats = new Stats()
+
+    constructor() {
+        document.body.appendChild(this.stats.dom)
+        this.clock = new Clock()
+
+        this.loader = new Loader()
+        this.camera = new Camera()
+        this.mainScene = new MainScene()
+        this.renderer = new Renderer(this.camera, this.mainScene)
+        this.player = new Player(this.loader)
+
+        this.npcManager = new NPCManager(this.loader);
+
+        // Adiciona o grupo com todos os NPCs à cena
+        // this.mainScene.scene.add(this.npcManager.group);
+
+        // this.npcManager.spawn({ x: -2, y: .5, z: 0 });
+        // this.npcManager.spawn({ x: -3, y: .5, z: -8 });
+
+        this.loader.loader.load("/models/glTF/rocky_path.glb", (gltf) => {
+            const model = gltf.scene
+            model.scale.set(15.0, 15.0, 15.0)
+            model.position.y = -10
+            model.traverse((child) => {
+                if (child instanceof Mesh && child.geometry) {
+                    child.geometry.computeBoundsTree()
+                    child.material.wireframe = false
+                    this.collisionMeshes.push(child)
+                }
+            })
+
+            const directLight = new DirectionalLight(0xffffff, 1)
+            directLight.position.set(3, 6, -5)
+            directLight.castShadow = true
+            directLight.target = model
+            this.mainScene.scene.add(directLight)
+            this.mainScene.scene.add(new DirectionalLightHelper(directLight))
+            this.mainScene.scene.add(model)
+
+
+            this.player.position.set(2, 0, 0)
+            this.mainScene.scene.add(this.player)
+
+        })
+
+        this.loader.start(() => this.update())
+
+        window.addEventListener("mousedown", (event) => {
+            if (event.button === 0) {
+                this.player.isShooting = true
+            }
+        });
+
+        window.addEventListener("mouseup", (event) => {
+            if (event.button === 0) {
+                this.player.isShooting = false
+                this.player.isAttacking = false
+            }
+        });
+
+        window.addEventListener("keydown", (e) => this.keysPressed.add(e.key.toLowerCase()))
+        window.addEventListener("keyup", (e) => this.keysPressed.delete(e.key.toLowerCase()))
+    }
+
+    applyGravity(delta: number) {
+        if (!this.hit) {
+            this.velocityY += this.gravity * delta
+        } else {
+            this.velocityY = 0
+        }
+        this.playerImpulse.y = this.velocityY * delta
+    }
+
+    checkGroundCollision(): boolean {
+        const origin = this.player.position.clone()
+        origin.y += 0.5 // altura do ponto de origem do ray
+
+        this.raycaster.set(origin, this.down)
+        const intersects: any[] = []
+
+        this.collisionMeshes.forEach(mesh => {
+            const hits = this.raycaster.intersectObject(mesh, true)
+            if (hits.length > 0) {
+                intersects.push(...hits)
+            }
+        })
+
+        if (intersects.length > 0) {
+            const firstHit = intersects[0]
+            const surfaceY = firstHit.point.y
+
+            const playerHeight = 1.0
+            const playerFootY = this.player.position.y - playerHeight / 2
+            const distanceToGround = playerFootY - surfaceY
+            const threshold = 0.05
+
+            if (distanceToGround < 0) {
+                this.hit = true
+                const targetY = surfaceY + playerHeight / 2
+                this.player.position.y = MathUtils.lerp(this.player.position.y, targetY, 0.25)
+                this.velocityY = 0
+            } else if (distanceToGround < threshold) {
+                this.hit = true
+                const targetY = surfaceY + playerHeight / 2
+                this.player.position.y = MathUtils.lerp(this.player.position.y, targetY, 0.25)
+                this.velocityY = 0
+            } else {
+                this.hit = false
+            }
+            return true
+        } else {
+            this.hit = false
+            return false
+        }
+    }
+
+    update = () => {
+        requestAnimationFrame(this.update)
+        this.stats.update()
+        this.renderer.update()
+        const delta = this.clock.getDelta()
+
+        this.camera.update(this.player)
+        const npcsMeshes = this.npcManager.collectNpcMeshes(this.npcManager.npcs);
+        this.player.update(delta, npcsMeshes);
+        this.npcManager.update(delta);
+
+        // Rotação baseada na câmera
+        this.camera.perspectiveCamera.getWorldDirection(this.playerDirection)
+        this.playerDirection.y = 0
+        this.playerDirection.normalize()
+
+        const angle = Math.atan2(this.playerDirection.x, this.playerDirection.z)
+        this.quaternion.setFromAxisAngle(new Vector3(0, 1, 0), angle)
+
+        // Entrada do teclado (direção)
+
+        if (!this.player.isAttacking) {
+            if (this.keysPressed.has("w") && !this.keysPressed.has("shift")) {
+                this.playerImpulse.add(this.playerDirection.clone().multiplyScalar(this.velocity * 1.8 * delta))
+                this.player.setState("Walk", 1.0)
+                this.player.lastAction = "Idle"
+            } else if (this.keysPressed.has("w") && this.keysPressed.has("shift")) {
+                this.playerImpulse.add(this.playerDirection.clone().multiplyScalar(this.velocity * 4 * delta))
+                this.player.setState("Running", 1.1)
+                this.player.lastAction = "Idle"
+            } else if (this.keysPressed.has("s")) {
+                this.playerImpulse.add(this.playerDirection.clone().multiplyScalar(-this.velocity * 1.5 * delta))
+                this.player.setState("Backward", 1.0)
+                this.player.lastAction = "Idle"
+            } else if (this.keysPressed.has("a")) {
+                const side = new Vector3(this.playerDirection.z, 0, -this.playerDirection.x)
+                this.playerImpulse.add(side.clone().multiplyScalar((this.velocity * 2) * delta))
+                this.player.setState("WalkLeft", 1.0)
+                this.player.lastAction = "Idle"
+            } else if (this.keysPressed.has("d")) {
+                const side = new Vector3(-this.playerDirection.z, 0, this.playerDirection.x)
+                this.playerImpulse.add(side.clone().multiplyScalar((this.velocity * 2) * delta))
+                this.player.setState("WalkRight", 1.0)
+                this.player.lastAction = "Idle"
+            }
+            else if (this.player.isShooting) {
+                this.player.attack(this.mainScene.scene);
+                this.player.lastAction = "Idle";
+            }
+            else {              
+                this.player.setState(this.player.lastAction, 1.0)               
+            }
+
+           
+        }
+
+
+        this.player.quaternion.slerpQuaternions(
+            this.player.quaternion,
+            this.quaternion,
+            delta * 4
+        )
+
+        // Aplica gravidade antes de mover
+        this.applyGravity(delta)
+
+        // Move apenas em X e Z antes da checagem de colisão no Y
+        this.player.position.add(new Vector3(this.playerImpulse.x, 0, this.playerImpulse.z))
+
+        // Checa colisão no chão (e ajusta posição Y se necessário)
+        this.checkGroundCollision()
+
+        // Depois de ajustar o Y com gravidade e colisão, aplica o deslocamento Y
+        this.player.position.y += this.playerImpulse.y
+
+        // Limpa impulso
+        this.playerImpulse.set(0, 0, 0)
+        
+    }
+
+}
+
+new Experience()
+
---
+++ src/Player.ts
@@ -0,0 +1,271 @@
+import {
+    CapsuleGeometry,
+    Mesh,
+    MeshBasicMaterial,
+    Object3D,
+    AnimationMixer,
+    AnimationAction,
+    Raycaster,
+    Vector3,
+    Scene,
+    CylinderGeometry,
+    LoopRepeat,
+    Quaternion,
+    MathUtils
+} from "three";
+import Loader from "./Loader";
+import NPC from "./NPC";
+import GUI from "three/examples/jsm/libs/lil-gui.module.min.js";
+
+class Player extends Object3D {
+    radius = 0.5;
+    capSegments = 10;
+    color = "#0000ff";
+    height = 2;
+    radialSegments = 30;
+    loader: Loader;
+    model?: Object3D;
+
+    mixer!: AnimationMixer;
+    clips: { [key: string]: AnimationAction } = {};
+    currentAction: AnimationAction | null = null;
+    currentState: string = "";
+
+    collisionMeshes: Mesh[] = [];
+    raycaster = new Raycaster();
+    down = new Vector3(0, 0, -1);
+
+    isAttacking = false;
+    attackCooldown = 0.15;
+    attackTimer = 0;
+    isShooting = false;
+
+    lastAction = "Idle";
+    isLoadedModel: Promise<void>;
+    handObject: Object3D = new Object3D();
+    sceneUpdateCallbacks: Set<(delta: number) => void> = new Set();
+
+    // Balas
+    activeBullets: { mesh: Mesh, direction: Vector3, distance: number }[] = [];
+    bulletSpeed = 50; // unidades por segundo
+    bulletMaxDistance = 100;
+
+    constructor(loader: Loader) {
+        super();
+        this.loader = loader;
+        this.isLoadedModel = this.loadModel();
+
+        this.setWeapon();
+    }
+
+    async loadModel() {
+        return await new Promise<void>((resolve) => {
+            this.loader.loader.load("/models/cowboy@model.glb", async (gltf) => {
+                const capsule = new Mesh(
+                    new CapsuleGeometry(this.radius, this.height, this.capSegments, this.radialSegments),
+                    new MeshBasicMaterial({ color: this.color, wireframe: true, visible: false })
+                );
+                capsule.scale.set(1, 1, 1);
+                this.add(capsule);
+
+                this.model = gltf.scene;
+                this.model.scale.set(2, 2, 2);
+                capsule.position.y = (this.height + 0.3 * this.radius) / 2;
+                this.model.position.y = -(this.height * this.radius) / 2;
+
+                this.model.traverse((child) => {
+                    if (child instanceof Mesh && child.geometry) {
+                        child.geometry.computeBoundsTree();
+                        child.material.wireframe = false;
+                        this.collisionMeshes.push(child);
+                    }
+                });
+
+                this.add(this.model);
+
+                // Mixer de animação
+                this.mixer = new AnimationMixer(this.model);
+
+                for (let animationKey in this.loader.globalAnimations) {
+                    this.clips[animationKey] = this.mixer.clipAction(this.loader.globalAnimations[animationKey]);
+                }
+
+                if (gltf.animations.length > 0) {
+                    this.clips["Idle"] = this.mixer.clipAction(gltf.animations[0]);
+                }
+
+                if (this.clips["Idle"]) {
+                    this.setState("Idle", 1.0);
+                }
+
+                resolve();
+            });
+        });
+    }
+
+    attack(scene: Scene) {
+        if (this.isAttacking) return;
+
+        this.isAttacking = true;
+        const action = this.setState("FireRifle", 1.0);
+        if (!action) return;
+
+        action.timeScale = 5.0;
+        action.setLoop(LoopRepeat, Infinity);
+        action.clampWhenFinished = true;
+        action.play();
+
+        if(action.isRunning())
+        {
+         this.shoot(scene);
+        this.attackTimer = 0; // começa o cooldown
+        }
+     
+    }
+
+
+
+    shoot(scene: Scene) {
+        const bulletGeo = new CylinderGeometry(0.05, 0.05, 0.3, 8);
+        const bulletMat = new MeshBasicMaterial({ color: 0xffd700 });
+        const bulletMesh = new Mesh(bulletGeo, bulletMat);
+        bulletMesh.rotation.x = Math.PI / 2;
+
+        // 1️⃣ Pegamos o objeto da arma
+        const rifle = this.model?.getObjectByName("rifle");
+
+
+        // 2️⃣ Criar um ponto no local do cano (exemplo: ponta da arma)
+        const muzzleLocal = new Vector3(0, 0, .5); // depende do modelo, ajuste até ficar no cano
+        const muzzleWorld = muzzleLocal.clone();
+        rifle?.localToWorld(muzzleWorld);
+
+        // 3️⃣ Posicionar a bala na ponta do cano
+        bulletMesh.position.copy(muzzleWorld);
+
+        const weaponDir = new Vector3(0, 0, 1);
+        const worldQuat = new Quaternion();
+        rifle?.getWorldQuaternion(worldQuat); // ✅ retorna um Quaternion real
+        weaponDir.applyQuaternion(worldQuat); // ✅ aplica rotação correta
+
+
+        const correction = new Quaternion().setFromAxisAngle(new Vector3(1, 0, 0), MathUtils.degToRad(-6));
+        weaponDir.applyQuaternion(correction);
+
+        weaponDir.normalize();
+        // 5️⃣ Adiciona à cena
+        scene.add(bulletMesh);
+
+        // 6️⃣ Salva com direção correta
+        this.activeBullets.push({
+            mesh: bulletMesh,
+            direction: weaponDir,
+            distance: 0
+        });
+
+    }
+
+    
+    update(delta: number, npcs?: Mesh[]) {
+
+        // Atualiza cooldown
+        if (this.attackTimer > 0) {
+            this.attackTimer -= delta;
+        }
+
+        // Se o player estiver segurando o tiro e cooldown zerou
+        if (this.isShooting && this.attackTimer <= 0) {
+            this.shoot(this.parent as Scene); // ou passe a cena como parâmetro do update
+            this.attackTimer = this.attackCooldown; // reseta cooldown
+        }
+
+        // Atualiza animações
+        if (this.mixer) this.mixer.update(delta);
+
+        // Atualiza balas
+        if (npcs) {
+            for (let i = this.activeBullets.length - 1; i >= 0; i--) {
+                const bullet = this.activeBullets[i];
+
+                // Move a bala
+                const moveStep = bullet.direction.clone().multiplyScalar(this.bulletSpeed * delta);
+                bullet.mesh.position.add(moveStep);
+                bullet.distance += moveStep.length();
+
+                // Checa colisão
+                this.raycaster.set(bullet.mesh.position, bullet.direction);
+                const hits = this.raycaster.intersectObjects(npcs, true);
+
+                if (hits.length > 0 && hits[0].distance < 0.5) {
+                    const hit = hits[0];
+
+                    if (hit.object.userData.type === "npc") {
+                        const npc = hit.object.userData.parentNpc as NPC;
+                        npc.takeDamage(30);
+                    }
+
+                    bullet.mesh.removeFromParent();
+                    this.activeBullets.splice(i, 1);
+                    continue;
+                }
+
+                // Remove bala se ultrapassou a distância máxima
+                if (bullet.distance > this.bulletMaxDistance) {
+                    bullet.mesh.removeFromParent();
+                    this.activeBullets.splice(i, 1);
+                }
+            }
+        }
+    }
+
+    setWeapon() {
+        this.isLoadedModel.then(async () => {
+            let gltf = await this.loader.loader.loadAsync("models/rifle.glb");
+            this.handObject = gltf.scene;
+            this.handObject.name = "rifle";
+
+            const handBone = this.model?.getObjectByName("mixamorigRightHand");
+            if (handBone) {
+                this.handObject.position.set(0, 0, 0);
+                this.handObject.rotation.set(0, 0, 0);
+                this.handObject.scale.set(0.06, 0.06, 0.06);
+
+                handBone.attach(this.handObject);
+                this.handObject.position.set(0.05, 0.23, 0.05);
+                this.handObject.rotation.set(1.753, Math.PI, 1.521);
+
+                const f = new GUI().addFolder("Weapon");
+                f.add(this.handObject.position, "x", -50, 50, 0.1);
+                f.add(this.handObject.position, "y", -50, 50, 0.1);
+                f.add(this.handObject.position, "z", -50, 50, 0.1);
+
+                f.add(this.handObject.rotation, "x", -Math.PI, Math.PI);
+                f.add(this.handObject.rotation, "y", -Math.PI, Math.PI);
+                f.add(this.handObject.rotation, "z", -Math.PI, Math.PI);
+
+                this.handObject.visible = true;
+            }
+        });
+    }
+
+    setState(name: string, speed: number): AnimationAction | null {
+        if (this.currentState === name || !this.clips[name]) return null;
+
+        const clip = this.clips[name];
+        const newAction = clip;
+        newAction.timeScale = speed;
+
+        if (this.currentAction) {
+            this.currentAction.fadeOut(0.3);
+        }
+
+        newAction.reset().fadeIn(0.3).play();
+
+        this.currentAction = newAction;
+        this.currentState = name;
+
+        return newAction;
+    }
+}
+
+export default Player;
